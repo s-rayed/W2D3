62d2dfcbf6d77b215cbe3d8f8ff451afb6d438b7
Rayed <siddiqui.rayed@gmail.com>
2016-05-06 16:31:07 -0400

    completed

---
+++ lighthouse-test-oop-retake2_rev1 2/lib/board.rb
@@ -0,0 +1,23 @@
+class Board
+
+  MAX_X = 9
+
+  # Types of Ship
+  BATTLESHIP = 'B'
+  DESTROYER  = 'D'
+  CRUISER    = 'C'
+  EMPTY      = '~'
+
+  # The grid below represents the game grid. It's a hash of arrays of strings. Don't let the weird syntax confuse you. It's just a fancy way of defining an array of strings. Each row is actual an array of strings. Each spot is either empty (~) or has a ship: Battleship (B), Destroyer(D), or Cruiser(C).
+  GRID = {
+    #     0 1 2 3 4 5 6 7 8 9
+    A: %w{~ ~ ~ C ~ ~ ~ B ~ ~},
+    B: %w{~ ~ ~ C ~ ~ ~ B ~ ~},
+    C: %w{~ ~ ~ ~ ~ ~ ~ B ~ ~},
+    D: %w{~ ~ ~ ~ ~ ~ ~ B ~ ~},
+    E: %w{~ ~ C C ~ ~ ~ ~ ~ ~},
+    F: %w{~ ~ ~ ~ ~ D D D D ~},
+    G: %w{~ ~ ~ ~ ~ ~ ~ ~ ~ ~}
+  }
+
+end
---
+++ lighthouse-test-oop-retake2_rev1 2/lib/cruiser.rb
@@ -0,0 +1,3 @@
+class Cruiser
+
+end
